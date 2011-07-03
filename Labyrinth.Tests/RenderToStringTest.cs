e7941dabed531f1d9ed6a224dd1c1567e74921a5
Markus Mayer <development@widemeadows.de>
2011-07-03 09:21:24 +0200

    Unit-Tests angepasst auf Randomized Prim

---
+++ Labyrinth.Tests/RenderToStringTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Labyrinth.Tests
+{
+	[TestFixture]
+	public class RenderToStringTest
+	{
+		[Test]
+		public void TestRecursiveBacktracker()
+		{
+			IMazeGenerator generator = new RecursiveBacktracker4();
+			var labyrinth = generator.Generate(30, 10);
+			string rendered = labyrinth.RenderToString();
+			Console.WriteLine(rendered);
+		}
+
+		[Test]
+		public void TestRandomizedPrim()
+		{
+			IMazeGenerator generator = new RandomizedPrim4();
+			var labyrinth = generator.Generate(30, 10);
+			string rendered = labyrinth.RenderToString();
+			Console.WriteLine(rendered);
+		}
+	}
+}
