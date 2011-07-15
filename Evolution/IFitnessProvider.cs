da4893a43aa353a274017ce390ef2fef7bc3d43b
Markus Mayer <development@widemeadows.de>
2011-07-15 22:40:12 +0200

    Elemente können verhungern

--- Evolution/IFitnessProvider.cs
+++ Evolution/IFitnessProvider.cs
@@ -5,6 +5,12 @@
 	/// </summary>
 	public interface IFitnessProvider
 	{
+        /// <summary>
+        /// Ermittelt, ob das Element verhungert ist, d.h. unabhängig
+        /// von der Fitness aus dem Pool genommen werden soll.
+        /// </summary>
+	    bool IsStarved { get; }
+
 		/// <summary>
 		/// Bezieht die Fitness.
 		/// Höhere Werte bedeuten höhere Fitness.
