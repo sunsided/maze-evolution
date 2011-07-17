2a97aab29ca6e585db320a0a9beeadac0cb55c6f
Markus Mayer <development@widemeadows.de>
2011-07-17 13:18:19 +0200

    Copyrights gesetzt

--- Evolution/Properties/AssemblyInfo.cs
+++ Evolution/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Evolution")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Evolutionary Programming Layer")]
+[assembly: AssemblyDescription("Bibliothek für evolutionäre Programmierung")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Evolution")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Markus Mayer")]
+[assembly: AssemblyProduct("Evolutionary Programming Layer")]
+[assembly: AssemblyCopyright("Copyright © Markus Mayer 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- MazeEvolution/Properties/AssemblyInfo.cs
+++ MazeEvolution/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MazeEvolution")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Evolutionary Programming Testbed")]
+[assembly: AssemblyDescription("Testanwendung zur Evaluierung von genetischer Programmierung")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Markus Mayer")]
 [assembly: AssemblyProduct("MazeEvolution")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Markus Mayer 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
